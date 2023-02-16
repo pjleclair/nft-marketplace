644863f55ef2c36caae7977512e25286aedf21ee
lompdoctor <pleclair@protonmail.com>
2023-02-16 17:14:02 -0600

    Scaffold Panel in Bid, style Panel

--- src/components/bid/bid.js
+++ src/components/bid/bid.js
@@ -3,6 +3,30 @@ import NFT from "./images/NFT.svg"
 import Avatar from "./images/Avatar.svg"
 
 const Bid = () => {
+
+    const Panel = () => {
+        return(
+            <div className="activity--panel">
+                <input type="checkbox"/>
+                <div className="nft--container">
+                    <img id="nft" src={NFT} alt="nft"/>
+                    <div className="nft--details">
+                        <div id="name">Cute Cube Cool</div>
+                        <div>John Abraham</div>
+                    </div>
+                </div>
+                <div>0.0025 ETH</div>
+                <div>0.0025 ETH</div>
+                <div className="offer-container">
+                    <img src={Avatar} alt="user"/>
+                    <div>0.0025 ETH</div>
+                </div>
+                <div>2 Hours 1 Min 30s</div>
+                <div id="X">X</div>
+            </div>
+        )
+    }
+
     return (
         <main className="bids--container">
             <div>
@@ -28,31 +52,24 @@ const Bid = () => {
             </div>
             <div className="activity--container">
                 <div className="activity--label">
-                    <div></div>
+                    <input type="checkbox"/>
                     <div>Item List</div>
                     <div>Open Price</div>
                     <div>Your Offer</div>
                     <div>Recent Offer</div>
                     <div>Time Left</div>
-                    <div>Action</div>
+                    <div id="X">Action</div>
                 </div>
-                <div className="activity--panel">
-                    <div></div>
-                    <div className="nft--container">
-                        <img id="nft" src={NFT} alt="nft"/>
-                        <div className="nft--details">
-                            <div id="name">Cute Cube Cool</div>
-                            <div>John Abraham</div>
-                        </div>
-                    </div>
-                    <div>0.0025 ETH</div>
-                    <div>0.0025 ETH</div>
-                    <div className="offer-container">
-                        <img src={Avatar} alt="user"/>
-                        <div>0.0025 ETH</div>
-                    </div>
-                    <div>2 Hours 1 Min 30s</div>
-                    <div>X</div>
+                <div className="activity--div"></div>
+                <div className="panel--container">
+                    <Panel />
+                    <Panel />
+                    <Panel />
+                    <Panel />
+                    <Panel />
+                    <Panel />
+                    <Panel />
+                    <Panel />
                 </div>
             </div>
         </main>
