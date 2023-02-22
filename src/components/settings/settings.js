21dc196318a0df4314bd7a91462406c409c518b9
lompdoctor <pleclair@protonmail.com>
2023-02-22 16:00:35 -0600

    Scaffold and begin styling settings

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -2,8 +2,83 @@ import "./settings.css"
 
 const Settings = () => {
     return (
-        <div style={{color:"white"}}>Test 4</div>
-    )
-}
+        <main className="collection--container">
+            <div>
+                <h1>Settings</h1>
+                <div className="label--container">
+                    <div>Welcome to your Settings</div>
+                    <div id="label">
+                        <div id="home">Home</div>
+                        <div>{`>`}</div>
+                        <div id="collections">Settings</div>
+                    </div>
+                </div>
+                <div id="selector">
+                    <div id="selected" className="selector--item">Profile</div>
+                    <div className="selector--item">Application</div>
+                    <div className="selector--item">Security</div>
+                    <div className="selector--item">Activity</div>
+                    <div className="selector--item">Payment Method</div>
+                    <div className="selector--item">API</div>
+                </div>
+                <div className="user--container">
+                    <div>
+                        <div>User profile</div>
+                        <div>
+                            <div>Full name</div>
+                            <input />
+                            <div>
+                                <img alt="pfp"/>
+                                <div>
+                                    <div>John Smith</div>
+                                    <div>Welcome setting page</div>
+                                </div>
+                            </div>
+                            <button>Save</button>
+                        </div>
+                    </div>
+                    <div>
+                        <div>Update profile</div>
+                        <div>
+                            <div>Email</div>
+                            <input />
+                            <div>Password</div>
+                            <input />
+                        </div>
+                        <button>Save</button>
+                    </div>
+                </div>
+                <div className="personal--info">
+                    <div className="info--container">
+                        <div>
+                            <div>Info</div>
+                            <input />
+                        </div>
+                        <div>
+                            <div>Info</div>
+                            <input />
+                        </div>
+                        <div>
+                            <div>Info</div>
+                            <input />
+                        </div>
+                        <div>
+                            <div>Info</div>
+                            <input />
+                        </div>
+                        <div>
+                            <div>Info</div>
+                            <input />
+                        </div>
+                        <div>
+                            <div>Info</div>
+                            <input />
+                        </div>
+                    </div>
+                    <button>Save</button>
+                </div>
+            </div>
+        </main>
+)}
 
 export default Settings;
