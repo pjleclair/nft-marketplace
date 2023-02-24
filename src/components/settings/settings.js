4b949dc72783186d0ed92f958fcdf862a908be22
lompdoctor <pleclair@protonmail.com>
2023-02-24 15:34:40 -0600

    Complete Settings styling

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,10 +1,11 @@
 import "./settings.css"
+import pfp from "./images/User.svg"
 
 const Settings = () => {
     return (
         <main className="collection--container">
-            <div>
-                <h1>Settings</h1>
+            <div className="page-header">
+                <h4>Settings</h4>
                 <div className="label--container">
                     <div>Welcome to your Settings</div>
                     <div id="label">
@@ -13,75 +14,75 @@ const Settings = () => {
                         <div id="collections">Settings</div>
                     </div>
                 </div>
-                <div className="settings" id="selector">
-                    <div id="selected" className="selector--item">Profile</div>
-                    <div className="selector--item">Application</div>
-                    <div className="selector--item">Security</div>
-                    <div className="selector--item">Activity</div>
-                    <div className="selector--item">Payment Method</div>
-                    <div className="selector--item">API</div>
-                </div>
-                <div className="user-update--container">
-                    <div className="user--container">
-                        <div id="label">User profile</div>
-                        <div className="user-info">
-                            <div>
-                                <div>Full name</div>
-                                <input />
-                                <div>
-                                    <img alt="pfp"/>
-                                    <div>
-                                        <div>John Smith</div>
-                                        <div>Welcome setting page</div>
-                                    </div>
+            </div>
+            <div className="settings" id="selector">
+                <div id="selected" className="selector--item">Profile</div>
+                <div className="selector--item">Application</div>
+                <div className="selector--item">Security</div>
+                <div className="selector--item">Activity</div>
+                <div className="selector--item">Payment Method</div>
+                <div className="selector--item">API</div>
+            </div>
+            <div className="user-update--container">
+                <div className="user--container">
+                    <h4 id="label">User profile</h4>
+                    <div className="user-info">
+                        <div>
+                            <div>Full name</div>
+                            <input />
+                            <div className="pfp-container">
+                                <img src={pfp} alt="pfp"/>
+                                <div id="user">
+                                    <h5>John Smith</h5>
+                                    <div>Welcome setting page</div>
                                 </div>
-                                <button>Save</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="profile--container">
-                        <div id="label">Update profile</div>
-                        <div className="update-profile">
-                            <div>
-                                <div>Email</div>
-                                <input />
-                                <div>Password</div>
-                                <input />
                             </div>
                             <button>Save</button>
                         </div>
                     </div>
                 </div>
-                <div className="personal--info">
-                    <div>Personal Information</div>
-                    <div className="info--container">
+                <div className="profile--container">
+                    <h4 id="label">Update profile</h4>
+                    <div className="update-profile">
                         <div>
-                            <div>Info</div>
+                            <div>Email</div>
+                            <input />
+                            <div>Password</div>
                             <input />
                         </div>
-                        <div>
-                            <div>Info</div>
-                            <input />
-                        </div>
-                        <div>
-                            <div>Info</div>
-                            <input />
-                        </div>
-                        <div>
-                            <div>Info</div>
-                            <input />
-                        </div>
-                        <div>
-                            <div>Info</div>
-                            <input />
-                        </div>
-                        <div>
-                            <div>Info</div>
-                            <input />
-                        </div>
+                        <button>Save</button>
                     </div>
-                    <button>Save</button>
                 </div>
+            </div>
+            <div className="personal--info">
+                <h4>Personal Information</h4>
+                <div className="info--container">
+                    <div id="info">
+                        <div>Info</div>
+                        <input />
+                    </div>
+                    <div id="info">
+                        <div>Info</div>
+                        <input />
+                    </div>
+                    <div id="info">
+                        <div>Info</div>
+                        <input />
+                    </div>
+                    <div id="info">
+                        <div>Info</div>
+                        <input />
+                    </div>
+                    <div id="info">
+                        <div>Info</div>
+                        <input />
+                    </div>
+                    <div id="info">
+                        <div>Info</div>
+                        <input />
+                    </div>
+                </div>
+                <button>Save</button>
             </div>
         </main>
 )}
